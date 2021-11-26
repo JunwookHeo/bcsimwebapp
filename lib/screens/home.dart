4834dca2b65f4965e3ed61c9b88b86c49e641f05
junwookheo <junwookheo@gamil.com>
2021-11-26 16:47:12 +1000

    tree view

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,9 +1,9 @@
-import 'dart:async';
 import 'dart:convert';
 
 import "package:collection/collection.dart";
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
+import 'package:tree_view/tree_view.dart';
 
 class MyHomePage extends StatefulWidget {
   final String title;
@@ -20,25 +20,46 @@ class _MyHomePageState extends State<MyHomePage> {
     Uri.parse('ws://localhost:8080/nodes'),
   );
 
-  Widget _buildListTile(List<ListItem> items, int index) {
-    final item = items[index];
-
-    return ListTile(
-      title: item.buildTitle(context),
-      subtitle: item.buildSubtitle(context),
-    );
+  List<Node> _makeGUINodeList(Map<int, List<Node>> mapNodes){
+    List<Node> nodes = [];
+    for (var ns in mapNodes.values) {
+      nodes.add( Node(sc:ns[0].sc, isSC:true, childNodes: ns ));
+      // for (var n in ns) {
+      //   nodes.add(n);
+      // }
+    }
+    return nodes;
   }
 
-  List<ListItem> _genListItems(Map<int, List<Node>> mapNodes) {
-    List<ListItem> listitems = [];
+  List<Widget> _genTreeItems(Map<int, List<Node>> mapNodes) {
+    List<Widget> listitems = [];
     for (var ns in mapNodes.values) {
-      listitems.add(HeadingItem(ns[0].sc));
+      listitems.add(StorageClassWidget(sc:ns[0].sc));
       for (var n in ns) {
-        listitems.add(NodeItem(n));
+        listitems.add(NodeWidget(node:n));
       }
     }
     return listitems;
   }
+
+  List<Widget> _getChildList(List<Node> nodes) {
+    return nodes.map((node) {
+      if (node.isSC) {
+        return Container(
+          margin: EdgeInsets.only(left: 8),
+          child: TreeViewChild(
+            parent: StorageClassWidget(sc:node.sc),
+            children: _getChildList(node.childNodes),
+          ),
+        );
+      }
+      return Container(
+        margin: const EdgeInsets.only(left: 4.0),
+        child: NodeWidget(node:node),
+      );
+    }).toList();
+  }
+
 
   @override
   Widget build(BuildContext context) {
@@ -83,12 +104,11 @@ class _MyHomePageState extends State<MyHomePage> {
                   for (var n in nodeMap.values) {
                     n.sort((a, b) => a.hash.compareTo(b.hash));
                   }
-                  List<ListItem> listitems = _genListItems(nodeMap);
+                  List<Node> nodes_gui = _makeGUINodeList(nodeMap);
 
-                  return ListView.builder(
-                    itemCount: listitems.length,
-                    itemBuilder: (context, index) =>
-                        _buildListTile(listitems, index),
+                  return TreeView(
+                    startExpanded: true,
+                    children: _getChildList(nodes_gui),
                   );
                 }
               },
@@ -109,40 +129,58 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 }
 
-abstract class ListItem {
-  Widget buildTitle(BuildContext context);
-
-  Widget buildSubtitle(BuildContext context);
-}
-
-class HeadingItem implements ListItem {
+class StorageClassWidget extends StatelessWidget {
   final int sc;
+  final VoidCallback? onPressedNext;
 
-  HeadingItem(this.sc);
+  const StorageClassWidget({Key? key, required this.sc, this.onPressedNext,}) : super(key: key);
 
   @override
-  Widget buildTitle(BuildContext context) {
-    return Text(
-      "Storage Class : " + sc.toString(),
-      style: Theme.of(context).textTheme.headline6,
+  Widget build(BuildContext context) {
+    Widget titleWidget = Text("Storage Class " + sc.toString());
+    Icon folderIcon = const Icon(Icons.folder);
+
+    IconButton expandButton = IconButton(
+      icon: const Icon(Icons.navigate_next),
+      onPressed: onPressedNext,
+    );
+
+    return Card(
+      child: ListTile(
+        leading: folderIcon,
+        title: titleWidget,
+        trailing: expandButton,
+        onTap: (()=> print("Storage Class " + sc.toString())),
+      ),
     );
   }
-
-  @override
-  Widget buildSubtitle(BuildContext context) => const SizedBox.shrink();
 }
 
-class NodeItem implements ListItem {
+class NodeWidget extends StatelessWidget {
   final Node node;
-  NodeItem(this.node);
+
+  const NodeWidget({Key? key, required this.node}) : super(key: key);
 
   @override
-  Widget buildTitle(BuildContext context) =>
-      Text(node.ip + ":" + node.port.toString());
+  Widget build(BuildContext context) {
+    Widget nodeHashWidget = Text(node.hash);
+    Widget ipAddressWidget = Text(
+      node.ip + ":" + node.port.toString(),
+    );
+    Icon fileIcon = const Icon(Icons.insert_drive_file);
 
-  @override
-  Widget buildSubtitle(BuildContext context) => Text(node.hash);
+    return Card(
+      elevation: 0.0,
+      child: ListTile(
+        leading: fileIcon,
+        title: nodeHashWidget,
+        subtitle: ipAddressWidget,
+        onTap: (()=> print(node.hash)),
+      ),
+    );
+  }
 }
+
 
 List<Node> parseNodes(String data) {
   final parsed = json.decode(data).cast<Map<String, dynamic>>();
@@ -155,13 +193,18 @@ class Node {
   final String ip;
   final int port;
   final String hash;
+  final bool isSC;
+  final List<Node> childNodes;
 
   Node(
       {this.mode = "",
       this.sc = 0,
       this.ip = "",
       this.port = 0,
-      this.hash = ""});
+      this.hash = "",
+      this.isSC = false,
+      this.childNodes = const <Node>[],
+      });
 
   factory Node.fromJson(Map<String, dynamic> json) {
     return Node(
